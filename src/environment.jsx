bc64727dc61f7b46818b848102cf65e537854eeb
reuvin22 <reuvinhernandez66@gmail.com>
2026-03-21 12:58:17 +0800

    Update

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,7 +1,7 @@
 const environment = {
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
-  VITE_PUSHER_APP_KEY: import.meta.env.PUSHER_APP_KEY,
-  VITE_PUSHER_APP_CLUSTER: import.meta.env.PUSHER_APP_CLUSTER
+  VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
+  VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER
 };
 
 export default environment;
