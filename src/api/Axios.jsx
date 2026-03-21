7aa3f1ba7c089e43f48a0e7b67cf91a691247dd5
reuvin22 <reuvinhernandez66@gmail.com>
2026-03-21 12:54:03 +0800

    Update

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -8,7 +8,7 @@ const axiosApi = axios.create({
     Accept: "application/json",
   },
 });
-//test
+
 axiosApi.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
--- src/echo.jsx
+++ src/echo.jsx
@@ -6,8 +6,8 @@ window.Pusher = Pusher;
 
 const echo = new Echo({
   broadcaster: "pusher",
-  key: environment.VITE_PUSHER_APP_KEY,
-  cluster: environment.VITE_PUSHER_APP_CLUSTER,
+  key: import.meta.env.PUSHER_APP_KEY,
+  cluster: import.meta.env.PUSHER_APP_CLUSTER,
   forceTLS: true,
 });
 
