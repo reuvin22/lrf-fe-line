e333cb063239d5ddbbd453387889a241cd2a3dff
Hernandez, Reuvin <127575733+reuvin22@users.noreply.github.com>
2026-05-12 05:57:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
   server: {
     proxy: {
       '/lrf-be': {
@@ -15,6 +13,4 @@ export default defineConfig({
       },
     },
   },
-=======
->>>>>>> e625a39c660817f17650ec74614882cc78ced7e1
 })
