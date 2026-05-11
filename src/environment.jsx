e625a39c660817f17650ec74614882cc78ced7e1
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2026-05-12 04:19:57 +0800

    Update

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,22 +1,4 @@
 const environment = {
-<<<<<<< HEAD
-  VITE_API_BASE_URL: 'https://miraboes.com/lrf-be/public/api/v1/',
-  VITE_PUSHER_APP_KEY: '68bcf13e240247effafd',
-  VITE_PUSHER_APP_CLUSTER: 'ap3',
-  VITE_LIFF_KEY: '2009308974-2bAhXIte'
-};
-
-export default environment;
-
-// const environment = {
-//   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
-//   VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
-//   VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//   VITE_LIFF_KEY: import.meta.env.VITE_LIFF_KEY
-// };
-
-// export default environment
-=======
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
   VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
@@ -24,4 +6,3 @@ export default environment;
 };
 
 export default environment
->>>>>>> 86ea2c35324079515dc82823d5a8795f32d801b4
