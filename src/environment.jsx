a222529e1503edc88c520a33c255362148414a1c
Reuvin <reuvinhernandez22@gmail.com>
2026-05-31 21:08:59 +0800

    Update

--- src/environment.jsx
+++ src/environment.jsx
@@ -4,7 +4,7 @@ const environment = {
   VITE_PUSHER_APP_KEY: '68bcf13e240247effafd',
   VITE_PUSHER_APP_CLUSTER: 'ap3',
   VITE_LIFF_KEY: '2009308974-2bAhXIte',
-  VITE_LIFF_ENABLED: false,
+  VITE_LIFF_ENABLED: true,
   VITE_LIFF_MOCK_USER_ID: 'local-test-user',
   VITE_LIFF_MOCK_DISPLAY_NAME: 'Local Test',
 };
