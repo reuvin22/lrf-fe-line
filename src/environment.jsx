ae04cb81c14a3cf4232688e2586c304e1ceadc2e
Reuvin <reuvinhernandez22@gmail.com>
2026-06-28 19:25:06 +0800

    Environment

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,12 +1,11 @@
 const environment = {
-  VITE_API_BASE_URL: '/lrf-be/public/api/v1/',
-  // VITE_API_BASE_URL: 'http://127.0.0.1:8000/api/v1/',
-  VITE_PUSHER_APP_KEY: '68bcf13e240247effafd',
-  VITE_PUSHER_APP_CLUSTER: 'ap3',
-  VITE_LIFF_KEY: '2009308974-2bAhXIte',
-  VITE_LIFF_ENABLED: true,
-  VITE_LIFF_MOCK_USER_ID: 'local-test-user',
-  VITE_LIFF_MOCK_DISPLAY_NAME: 'Local Test',
+  VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
+  VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+  VITE_LIFF_KEY: import.meta.env.VITE_LIFF_KEY,
+  VITE_LIFF_ENABLED: "true",
+  VITE_LIFF_MOCK_USER_ID: "local-test-user",
+  VITE_LIFF_MOCK_DISPLAY_NAME: "Local Test",
 };
 
-export default environment
+export default environment;
