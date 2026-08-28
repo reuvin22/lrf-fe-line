4d562346150969dbe3b115462beba0f9a18c27b5
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2026-08-28 14:27:55 +0800

    Update

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,5 +1,5 @@
 const environment = {
-  VITE_API_URL: import.meta.env.API_URL,
+  API_URL: import.meta.env.API_URL,
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
   VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
