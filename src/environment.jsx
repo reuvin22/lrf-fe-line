d5be45605ab7b08382244146d12e65d23190450a
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2026-08-28 14:22:47 +0800

    Update Axios

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,5 +1,5 @@
 const environment = {
-  VITE_API_URL: import.meta.env.API_URL || 'http://127.0.0.1:8000',
+  VITE_API_URL: import.meta.env.API_URL,
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
   VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
