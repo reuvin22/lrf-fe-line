ce6a9a5d0605aef90e92fb715334088989241f3c
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2026-08-28 14:23:50 +0800

    Update Axios

--- src/api/Axios.jsx
+++ src/api/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import environment from "../environment";
 
 const axiosApi = axios.create({
-  baseURL: `${environment.VITE_API_URL}${environment.VITE_API_BASE_URL}`,
+  baseURL: `${environment.API_URL}${environment.VITE_API_BASE_URL}`,
   timeout: 0,
   headers: {
     "Content-Type": "application/json",
