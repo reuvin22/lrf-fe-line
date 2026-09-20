70a6802d7cbefec368ab1ca91aa5d251bf90886e
Reuvin <reuvinhernandez22@gmail.com>
2026-09-20 08:39:14 +0800

    Env changes

--- src/environment.jsx
+++ src/environment.jsx
@@ -1,19 +1,8 @@
 const environment = {
-<<<<<<< Updated upstream
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_PUSHER_APP_KEY: import.meta.env.VITE_PUSHER_APP_KEY,
   VITE_PUSHER_APP_CLUSTER: import.meta.env.VITE_PUSHER_APP_CLUSTER,
   VITE_LIFF_KEY: import.meta.env.VITE_LIFF_KEY,
-=======
-  VITE_API_BASE_URL: '/lrf-be/public/api/v1/',
-  VITE_PUSHER_APP_KEY: '68bcf13e240247effafd',
-  VITE_PUSHER_APP_CLUSTER: 'ap3',
-  VITE_LIFF_KEY: '2009308974-2bAhXIte',
-  // Automatically false during `npm run dev`, true in production builds
-  VITE_LIFF_ENABLED: !import.meta.env.DEV,
-  VITE_LIFF_MOCK_USER_ID: 'local-test-user',
-  VITE_LIFF_MOCK_DISPLAY_NAME: 'Local Test',
->>>>>>> Stashed changes
 };
 
 export default environment;
